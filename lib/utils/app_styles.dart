bd9ec6ccdaccbef84ccbced4f9839664fe42a16d
ProsperChihimba <prosperchihimba@gmail.com>
2023-02-14 16:14:19 +0400

    initial design

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,12 @@
+import 'package:nala_clon/utils/app_layout.dart';
+import 'package:flutter/cupertino.dart';
+
+class Styles {
+  // App colors
+  static Color blueColor = const Color(0xFF04A4DD);
+
+  // App default styles
+  static TextStyle normalText = TextStyle(
+    fontSize: AppLayout.getHeight(14),
+  );
+}
