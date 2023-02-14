f857f8789fed9d9049dc19a5818231ff6613d70e
ProsperChihimba <prosperchihimba@gmail.com>
2023-02-14 16:27:35 +0400

    initial

---
+++ lib/bottom_bar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class BottomBarSection extends StatefulWidget {
+  const BottomBarSection({super.key});
+
+  @override
+  State<BottomBarSection> createState() => _BottomBarSectionState();
+}
+
+class _BottomBarSectionState extends State<BottomBarSection> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: SizedBox(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:nala_clon/bottom_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +11,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Text("data"),
+    return const CupertinoApp(
+      title: 'Nala iOS clone',
+      debugShowCheckedModeBanner: false,
+      theme: CupertinoThemeData(brightness: Brightness.light),
+      home: BottomBarSection(),
     );
   }
 }
