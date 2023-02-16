6e893153afa58b626c5e18fbf095913bb2336f14
ProsperChihimba <prosperchihimba@gmail.com>
2023-02-16 15:11:17 +0400

    Add: new features section

--- lib/send/index.dart
+++ lib/send/index.dart
@@ -25,6 +25,7 @@ class _SendPageState extends State<SendPage> {
               header(),
 
               // items
+              newFeature(context),
             ],
           ),
         )
@@ -42,6 +43,7 @@ class _SendPageState extends State<SendPage> {
   }
 }
 
+// header
 Widget header() {
   return Column(
     children: [
@@ -99,5 +101,64 @@ Widget header() {
       ),
       Gap(AppLayout.getHeight(25)),
     ],
+  );
+}
+
+// new feature
+Widget newFeature(BuildContext context) {
+  final size = AppLayout.getSize(context);
+  return Container(
+    padding: EdgeInsets.symmetric(
+      horizontal: AppLayout.getWidth(13),
+      vertical: AppLayout.getHeight(13),
+    ),
+
+    //
+    decoration: BoxDecoration(
+      color: CupertinoColors.systemGrey5,
+      borderRadius: BorderRadius.circular(AppLayout.getHeight(10)),
+    ),
+
+    //
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // header
+        Text(
+          "⭐️ New features",
+          style: Styles.normalText.copyWith(
+            color: Styles.blueColor.withOpacity(0.8),
+            fontWeight: FontWeight.w600,
+            fontSize: AppLayout.getHeight(16),
+          ),
+        ),
+
+        //
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            //
+            SizedBox(
+              width: size.width * 0.80,
+              child: Text(
+                "Buy goods and Pay bill are in early access. We are still testing their relibility",
+                textAlign: TextAlign.start,
+                style: Styles.normalText.copyWith(
+                  color: CupertinoColors.systemGrey,
+                  fontSize: AppLayout.getHeight(12),
+                ),
+              ),
+            ),
+
+            //
+            const Icon(
+              CupertinoIcons.exclamationmark_circle,
+              color: CupertinoColors.systemGrey,
+            ),
+          ],
+        )
+      ],
+    ),
   );
 }
