a0efc34e754f6aaf080019191bf7554ec8a9b385
ProsperChihimba <prosperchihimba@gmail.com>
2023-02-16 16:50:21 +0400

    Alert dialog design

--- lib/sections/alert_dialog.dart
+++ lib/sections/alert_dialog.dart
@@ -1,32 +1,65 @@
 import 'package:flutter/cupertino.dart';
+import 'package:gap/gap.dart';
+import 'package:nala_clon/sections/index.dart';
+import 'package:nala_clon/utils/app_layout.dart';
+import 'package:nala_clon/utils/app_styles.dart';
 
 void showAlertDialog(BuildContext context) {
   showCupertinoModalPopup<void>(
     context: context,
     builder: (BuildContext context) => CupertinoAlertDialog(
-      title: const Text('Alert'),
-      content: const Text('Proceed with destructive action?'),
-      actions: <CupertinoDialogAction>[
-        CupertinoDialogAction(
-          /// This parameter indicates this action is the default,
-          /// and turns the action's text to bold text.
-          isDefaultAction: true,
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('No'),
-        ),
-        CupertinoDialogAction(
-          /// This parameter indicates the action would perform
-          /// a destructive action such as deletion, and turns
-          /// the action's text color to red.
-          isDestructiveAction: true,
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Yes'),
-        ),
-      ],
+      content: Column(
+        children: [
+          // line
+          const GreyLine(
+            color: CupertinoColors.systemGrey4,
+          ),
+
+          // image
+          const Image(
+            image: AssetImage("assets/shop.png"),
+          ),
+
+          // header
+          Text(
+            "⭐️ New features ⭐️",
+            style: Styles.normalText.copyWith(
+              color: CupertinoColors.black.withOpacity(0.55),
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+
+          // desc
+          Gap(AppLayout.getHeight(15)),
+          Text(
+            "Wo-hoo! You are one of our first users to try this out. Please let us know how your experience went",
+            style: Styles.normalText.copyWith(
+              color: CupertinoColors.black.withOpacity(0.4),
+              fontSize: AppLayout.getHeight(13),
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+
+          // button
+          Gap(AppLayout.getHeight(30)),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: AppLayout.getWidth(10)),
+            decoration: BoxDecoration(
+              color: Styles.blueColor.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(AppLayout.getHeight(5)),
+            ),
+            child: Center(
+              child: Text(
+                "Okay",
+                style: Styles.normalText.copyWith(
+                  color: Styles.blueColor,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     ),
   );
 }
